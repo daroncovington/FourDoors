7cfdb5421d233dec7863139a176c60e3d12246d7
JoseMald07 <122644219+JoseMald07@users.noreply.github.com>
2023-02-07 14:40:52 -0600

    car controller

---
+++ FourDoors/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 60;
+    private float turnSpeed = 80;
+    private float horizontalInput;
+    private float verticalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
