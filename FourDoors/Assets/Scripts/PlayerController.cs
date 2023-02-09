91d6c0586fbf0536aa3cd3ee6d4cedf46f5e6427
JoseMald07 <122644219+JoseMald07@users.noreply.github.com>
2023-02-09 14:27:34 -0600

    car controller

--- FourDoors/Assets/Scripts/PlayerController.cs
+++ FourDoors/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 60;
     private float turnSpeed = 80;
+    private float reverseSpeed = 30;
     private float horizontalInput;
     private float verticalInput;
+    public bool inForward;
+    public bool inReverse;
+
+    private Animator playerAnim;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +26,29 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        
+        if(verticalInput > 0)
+        {
+            inForward = true;
+            inReverse = false;
+        }
+        else if (verticalInput < 0)
+        {
+            inForward = false;
+            inReverse = true;
+        }
 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
-        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        if(inForward)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        }
+       else if(inReverse)
+       {
+            transform.Translate(Vector3.forward * reverseSpeed * Time.deltaTime * verticalInput);
+            transform.Rotate(Vector3.down * turnSpeed * Time.deltaTime * horizontalInput);
+       }
+
+        
     }
 }
