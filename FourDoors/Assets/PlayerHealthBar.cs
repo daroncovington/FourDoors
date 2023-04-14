3a0954753be5f88857ac82830cdae89387c70682
daroncovington <122644170+daroncovington@users.noreply.github.com>
2023-04-14 14:18:07 -0500

    code

--- FourDoors/Assets/PlayerHealthBar.cs
+++ FourDoors/Assets/PlayerHealthBar.cs
@@ -4,39 +4,29 @@ using UnityEngine;
 
 public class PlayerHealthBar : MonoBehaviour
 {
-  public class ScriptExample : MonoBehaviour
-{
-    void DestroyGameObject()
+    public GameObject playerHealthBar;
+    public GameObject player;
+    public GameObject enemy;
+
+     void DestroyGameObject()
     {
-        Destroy(gameObject);
+        Destroy(Player);
+
+    }
+    
+    void Start()
+    {
+        playerHealthBar
     }
 
-    void DestroyScriptInstance()
-    {
-        // Removes this script instance from the game object
-        Destroy(this);
-    }
-
-    void DestroyComponent()
-    {
-        // Removes the rigidbody from the game object
-        Destroy(GetComponent<Rigidbody>());
-    }
-
-    void DestroyObjectDelayed()
-    {
-        // Kills the game object in 5 seconds after loading the object
-        Destroy(gameObject, 5);
-    }
-
-    // When the user presses Ctrl, it will remove the
-    // BoxCollider component from the game object
     void Update()
     {
-        if (Input.GetButton("Fire1") && GetComponent<BoxCollider>())
-        {
-            Destroy(GetComponent<BoxCollider>());
-        }
+        
     }
-}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (player collides into enemy)
+        
+    }
 }
